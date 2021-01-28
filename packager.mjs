6fc8bab68393ae00c5a96e305604bdfc3d849eb4
sosuisen <hidekazu.kubota@gmail.com>
2021-01-28 15:55:38 +0900

    [fix] Add icon for Windows control panel
    Add icon for Windows control panel
    
    Fix app name to be without - (dashes).
    See https://github.com/electron/windows-installer/issues/264
    
    Fix name of setup.exe
    
    Remove .msi

--- packager.mjs
+++ packager.mjs
@@ -1,23 +1,35 @@
 import packager from 'electron-packager';
 import { rebuild } from 'electron-rebuild';
 import electronInstaller from 'electron-winstaller';
+import packageJson from './package.json';
 
 const createInstaller = async () => {
 
   // Enter app name without dashes & spaces",
   const name = 'InventoryManager';
 
+  // Default App icon
+  // If the file extension of icon is omitted, its is auto-completed to the correct extension based on the platform.
+  const icon = 'assets/inventory_manager_icon'; 
+  
+  // A URL to an ICO file displayed in Control Panel > Programs and Features
+  // Defaults to the Atom icon.
+  // iconUrl must be a url that start with http or https. Squirrel cannot accept a local file.
+  const iconUrl = 'https://raw.githubusercontent.com/sosuisen/inventory-manager/main/assets/inventory_manager_icon.ico';
+  
+  const copyright = 'Copyright(C) 2021 Hidekazu Kubota';
+
   await packager({
     dir: '.',
     name: name,
-    appCopyright: "Copyright(C) 2021 Hidekazu Kubota",
+    appCopyright: copyright,
     asar: true,
-    icon: "assets/inventory_manager_icon",
+    icon: icon,
     overwrite: true,
     ignore: ['^(\/html|\/installer|\/inventory_manager_data|\/out|\/src)', '\.vscode|\.eslint.*|\.gitignore|tsconfig.*|webpack.*|packager.mjs|package-lock.json|config.json|README.md'],
     win32metadata: {
-      ProductName: "Inventory Manager",
-      FileDescription: "Inventory Manager"
+      ProductName: packageJson.productName,
+      FileDescription: packageJson.productName,
     },
     // … other options
     afterCopy: [(buildPath, electronVersion, platform, arch, callback) => {
@@ -33,7 +45,11 @@ const createInstaller = async () => {
       appDirectory: './InventoryManager-win32-x64',
       outputDirectory: './installer/',
       title: name,
-      exe: name + '.exe',
+      name: name, // name must be without - (dashes). See https://github.com/electron/windows-installer/issues/264
+      exe: `${name}.exe`,
+      iconUrl: iconUrl, // icon 
+      setupExe: `${name}-${packageJson.version}-Setup.exe`,
+      noMsi: true
     }).catch (e => console.log(`Error in Windows Installer: ${e.message}`));
 };
 
