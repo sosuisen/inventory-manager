f607b8e683fe3571cfb451ef3573fb8eabc4b189
sosuisen <hidekazu.kubota@gmail.com>
2021-01-29 02:56:14 +0900

    [add] Add preload and contextIsolation

---
+++ src/modules_renderer/window.ts
@@ -0,0 +1,17 @@
+/**
+ * Inventory Manager
+ * Copyright (c) Hidekazu Kubota
+ *
+ * This source code is licensed under the Mozilla Public License Version 2.0
+ * found in the LICENSE file in the root directory of this source tree.
+ */
+
+import { DatabaseCommand } from '../modules_common/types';
+
+interface WindowWithAPI extends Window {
+  api: {
+    db: (command: DatabaseCommand) => Promise<void>;
+  };
+}
+declare const window: WindowWithAPI;
+export default window;
---
+++ src/preload.ts
@@ -0,0 +1,26 @@
+/**
+ * Inventory Manager
+ * Copyright (c) Hidekazu Kubota
+ *
+ * This source code is licensed under the Mozilla Public License Version 2.0
+ * found in the LICENSE file in the root directory of this source tree.
+ */
+
+import { contextBridge, ipcRenderer, MouseInputEvent } from 'electron';
+import { DatabaseCommand } from './modules_common/types';
+
+contextBridge.exposeInMainWorld('api', {
+  /**
+   * Command from Renderer process
+   */
+  db: (command: DatabaseCommand) => {
+    return ipcRenderer.invoke('db', command);
+  },
+});
+
+/**
+ * Command from Main process
+ */
+// ipcRenderer.on('reactive-forward', (event, propertyName, doc) => {
+//  window.postMessage({ command: 'reactive-forward', propertyName, doc }, 'file://');
+// });
