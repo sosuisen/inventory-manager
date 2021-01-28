076032ffa6ea898bc9072370a451e1ea247005c8
sosuisen <hidekazu.kubota@gmail.com>
2021-01-29 02:58:54 +0900

    [fix] Fix close timing of GitDocumentDB

--- src/main.ts
+++ src/main.ts
@@ -76,16 +76,18 @@ app.on('ready', init);
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
+app.on('window-all-closed', async () => {
+  await gitDDB.close();
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on('activate', async () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
+    await gitDDB.open();
     createWindow();
   }
 });
