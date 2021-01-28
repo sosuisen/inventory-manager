a9a0e2ee679a9c4882a39b39d79ba8ad7b62e94c
sosuisen <hidekazu.kubota@gmail.com>
2021-01-28 14:35:46 +0900

    [add] Add electron-connect for hot reloading

--- webpack.renderer.development.js
+++ webpack.renderer.development.js
@@ -1,11 +1,20 @@
 const cssModulesScopedName = '[path]___[name]__[local]___[hash:base64:5]';
 
 const { merge } = require('webpack-merge');
+const createElectronReloadWebpackPlugin = require('electron-reload-webpack-plugin');
 const common = require('./webpack.renderer.common.js');
+const ElectronReloadWebpackPlugin = createElectronReloadWebpackPlugin({
+  // Path to `package.json` file with main field set to main process file path, or just main process file path
+  path: './',
+  // or just `path: './'`,
+  // Other 'electron-connect' options
+  logLevel: 0,
+});
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
+  plugins: [ElectronReloadWebpackPlugin('electron-renderer')],
 
   module: {
     rules: [
