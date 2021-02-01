3cb6063d6864b5314e1ae7caf684721c7c368c8f
sosuisen <hidekazu.kubota@gmail.com>
2021-02-01 14:54:23 +0900

    [fix] Rewind previous commit

--- webpack.renderer.development.js
+++ webpack.renderer.development.js
@@ -1,4 +1,7 @@
 const { merge } = require('webpack-merge');
+
+const cssModulesScopedName = '[path]___[name]__[local]___[hash:base64:5]';
+
 const createElectronReloadWebpackPlugin = require('electron-reload-webpack-plugin');
 const common = require('./webpack.renderer.common.js');
 const ElectronReloadWebpackPlugin = createElectronReloadWebpackPlugin({
@@ -13,4 +16,61 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   plugins: [ElectronReloadWebpackPlugin('electron-renderer')],
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: [/node_modules/],
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              plugins: [
+                // 3) .jsx => .js
+                'transform-react-jsx',
+                // 2) styleName in .jsx => className in .jsx
+                ['react-css-modules', { generateScopedName: cssModulesScopedName }],
+              ],
+            },
+          },
+          // 1) .ts => .js,  .tsx => .jsx
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: 'tsconfig.renderer.development.json',
+            },
+          },
+        ],
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          // import .css in .js
+          'style-loader',
+          {
+            /**
+             * Must use css-loader@3 because hash generator of css-loader@4 is different from that of 'react-css-modules'
+             * See https://github.com/webpack-contrib/css-loader/issues/877
+             */
+
+            // Apply css modules
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: cssModulesScopedName,
+              },
+              importLoaders: 1,
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
 });
