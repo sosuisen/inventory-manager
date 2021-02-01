01507598b8a2d547af4e45638cb3268a204f8f90
sosuisen <hidekazu.kubota@gmail.com>
2021-02-01 21:18:28 +0900

    [add] Load inventory from database

--- src/modules_renderer/selector.ts
+++ src/modules_renderer/selector.ts
@@ -3,6 +3,9 @@ import { InventoryState } from './store.types.inventory';
 export const selectorCurrentBoxId = (state: InventoryState) => state.work.currentBox;
 export const selectorCurrentBoxAndItems = (state: InventoryState) => {
   const box = state.box[state.work.currentBox];
-  const items = box ? box.items.map(_id => state.item[_id]) : [];
-  return { name: box.name, items: items };
+  if (box) {
+    const items = box.items.map(_id => state.item[_id]);
+    return { name: box.name, items: items };
+  }
+  return { name: '', items: [] };
 };
--- src/modules_renderer/store.types.inventory.ts
+++ src/modules_renderer/store.types.inventory.ts
@@ -52,9 +52,11 @@ export type InventoryState = {
  * Redux Actions
  */
 
+export const ITEM_INIT = 'item-init';
 export const ITEM_ADD = 'item-add';
 export const ITEM_UPDATE = 'item-update';
 export const ITEM_DELETE = 'item-delete';
+export const BOX_INIT = 'box-init';
 export const BOX_ADD = 'box-add';
 export const BOX_UPDATE = 'box-update';
 export const BOX_DELETE = 'box-delete';
@@ -62,6 +64,13 @@ export const BOX_ITEM_ADD = 'box-item-add';
 export const BOX_ITEM_DELETE = 'box-item-delete';
 export const WORK_CURRENT_BOX_ADD = 'work-current-box-status-add'; // NOTE: Adding status-current-box-status is same as updating it.
 export const WORK_CURRENT_BOX_UPDATE = 'work-current-box-status-update';
+
+export type ItemInitAction = {
+  type: typeof ITEM_INIT;
+  payload: {
+    [key: string]: Item;
+  };
+};
 
 export type ItemAddAction = {
   type: typeof ITEM_ADD;
@@ -83,6 +92,13 @@ export type ItemUpdateAction = {
 export type ItemDeleteAction = {
   type: typeof ITEM_DELETE;
   payload: string;
+};
+
+export type BoxInitAction = {
+  type: typeof BOX_INIT;
+  payload: {
+    [key: string]: Box;
+  };
 };
 
 export type BoxAddAction = {
@@ -132,9 +148,14 @@ export type WorkCurrentBoxUpdateAction = {
   payload: string;
 };
 
-export type ItemAction = ItemAddAction | ItemUpdateAction | ItemDeleteAction;
+export type ItemAction =
+  | ItemInitAction
+  | ItemAddAction
+  | ItemUpdateAction
+  | ItemDeleteAction;
 
 export type BoxAction =
+  | BoxInitAction
   | BoxAddAction
   | BoxUpdateAction
   | BoxDeleteAction
@@ -145,52 +166,7 @@ export type WorkAction = WorkCurrentBoxAddAction | WorkCurrentBoxUpdateAction;
 
 export type InventoryAction = ItemAction | BoxAction | WorkAction;
 
-export const initialItemState: ItemState = {
-  '1': {
-    _id: '1',
-    name: 'kimari',
-    takeout: false,
-    created_date: '2020-01-01 00:00:00',
-    modified_date: '2020-01-01 00:00:00',
-  },
-  '2': {
-    _id: '2',
-    name: 'shirase',
-    takeout: false,
-    created_date: '2020-01-01 00:00:00',
-    modified_date: '2020-01-01 00:00:00',
-  },
-  '10': {
-    _id: '10',
-    name: 'gin',
-    takeout: false,
-    created_date: '2020-01-01 00:00:00',
-    modified_date: '2020-01-01 00:00:00',
-  },
-};
-export const initialBoxState: BoxState = {
-  '1': {
-    _id: '1',
-    name: 'submember',
-    created_date: '2020-01-01 00:00:00',
-    modified_date: '2020-01-01 00:00:00',
-    items: ['1', '2'],
-  },
-  '2': {
-    _id: '2',
-    name: 'member',
-    created_date: '2020-01-01 00:00:00',
-    modified_date: '2020-01-01 00:00:00',
-    items: ['10'],
-  },
-};
 export const initialWorkState: WorkState = {
-  boxOrder: ['1', '2'],
+  boxOrder: ['1'],
   currentBox: '1',
-};
-
-export const initialInventoryState = {
-  item: initialItemState,
-  box: initialBoxState,
-  work: initialWorkState,
 };
