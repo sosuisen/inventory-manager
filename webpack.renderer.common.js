63b482aa8e5b5164d33ab912da99046b813dc911
sosuisen <hidekazu.kubota@gmail.com>
2021-02-01 14:54:23 +0900

    [fix] Rewind previous commit

--- webpack.renderer.common.js
+++ webpack.renderer.common.js
@@ -1,7 +1,5 @@
 const path = require('path');
 
-const cssModulesScopedName = '[path]___[name]__[local]___[hash:base64:5]';
-
 module.exports = {
   entry: './src/renderer.ts',
   target: 'web',
@@ -20,62 +18,6 @@ module.exports = {
   externals: {
     'react': 'React',
     'react-dom': 'ReactDOM',
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        exclude: [/node_modules/],
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              plugins: [
-                // 3) .jsx => .js
-                'transform-react-jsx',
-                // 2) styleName in .jsx => className in .jsx
-                ['react-css-modules', { generateScopedName: cssModulesScopedName }],
-              ],
-            },
-          },
-          // 1) .ts => .js,  .tsx => .jsx
-          {
-            loader: 'ts-loader',
-            options: {
-              configFile: 'tsconfig.renderer.development.json',
-            },
-          },
-        ],
-      },
-      {
-        enforce: 'pre',
-        test: /\.js$/,
-        loader: 'source-map-loader',
-      },
-      {
-        test: /\.css$/,
-        use: [
-          // import .css in .js
-          'style-loader',
-          {
-            /**
-             * Must use css-loader@3 because hash generator of css-loader@4 is different from that of 'react-css-modules'
-             * See https://github.com/webpack-contrib/css-loader/issues/877
-             */
-
-            // Apply css modules
-            loader: 'css-loader',
-            options: {
-              modules: {
-                localIdentName: cssModulesScopedName,
-              },
-              importLoaders: 1,
-              sourceMap: true,
-            },
-          },
-        ],
-      },
-    ],
+    'react-redux': 'ReactRedux',
   },
 };
