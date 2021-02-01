78f1d7122a3aee1fe8181bba5c3991056afcfec7
sosuisen <hidekazu.kubota@gmail.com>
2021-02-01 11:45:50 +0900

    [add] Add selector functions

--- src/modules_renderer/ItemRow.tsx
+++ src/modules_renderer/ItemRow.tsx
@@ -3,16 +3,16 @@ import { useDispatch, useSelector } from 'react-redux';
 import {
   BOX_ITEM_DELETE,
   BoxAction,
-  InventoryState,
   Item,
   ITEM_DELETE,
   ItemAction,
 } from './store.types.inventory';
 import './ItemRow.css';
 import { getLocalDateAndTime } from './utils';
+import { selectorCurrentBoxId } from './selector';
 
 export const ItemRow = (prop: { item: Item }) => {
-  const currentBoxId = useSelector((state: InventoryState) => state.work.currentBox);
+  const currentBoxId = useSelector(selectorCurrentBoxId);
   const dispatch = useDispatch();
 
   const deleteItem = useCallback(() => {
