d5a07fae9453ac9054ec123911a2300e55f70d4f
sosuisen <hidekazu.kubota@gmail.com>
2021-02-14 01:40:21 +0900

    fix: fix local date and time calculation

--- src/modules_common/utils.ts
+++ src/modules_common/utils.ts
@@ -8,12 +8,17 @@ export const getCurrentDateAndTime = (): string => {
 export const getLocalDateAndTime = (utcDateAndTime: string): string => {
   const regularUTC = utcDateAndTime.replace(
     /^(\d\d\d\d-\d\d-\d\d).(\d\d:\d\d:\d\d)/,
-    '$1 $2 GMT'
+    '$1 $2'
   );
   const offset = new Date().getTimezoneOffset();
   const utcMsec = Date.parse(regularUTC);
   const localMsec = utcMsec - offset * 60 * 1000;
-  return new Date(localMsec).toISOString().replace(/^(.+?)T(.+?)\..+?$/, '$1 $2');
+  const localDate = new Date(localMsec);
+  const zeroPad = (num: number) => ('00' + num).slice(-2);
+  return `${localDate.getFullYear()}/${zeroPad(localDate.getMonth() + 1)}/${zeroPad(
+    localDate.getDate()
+  )} ${zeroPad(localDate.getHours())}:${zeroPad(localDate.getMinutes())}`;
+  //  return new Date(localMsec).toISOString().replace(/^(.+?)T(.+?)\..+?$/, '$1 $2');
 };
 export const generateId = () => {
   return 'id' + nanoid(21); // 23 characters include only 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz-
